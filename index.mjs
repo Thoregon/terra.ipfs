0b2da56638a46a7e56a59a78f729780f610963c0
Bernhard Lukassen <bl@bernhard-lukassen.com>
2021-02-15 15:43:38 +0100

    aurora blueprint refactoring: rename all 'layout' to 'bluerprint'

---
+++ index.mjs
@@ -0,0 +1,11 @@
+/**
+ *
+ *
+ * @author: Bernhard Lukassen
+ * @licence: MIT
+ * @see: {@link https://github.com/Thoregon}
+ */
+
+import HeavyMatter from "./lib/heavymatter.mjs";
+
+export const service = new HeavyMatter();
